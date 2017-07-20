3c29ef450160e423ddea9f49aa7437914512f82f
Brian Vogelgesang <KidA001@gmail.com>
2017-07-20 14:14:01 -0700

    Settings page styling

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,9 +1,27 @@
-$(document).on('turbolinks:load', function() {
-  $('.switchery').each(function(index, elem) {
-    elem.remove();
-  });
+function phoneInput(event) {
+  if (event.keyCode == 8) return;
+  var phoneString = event.currentTarget.value.replace(/\D/g, "");
 
+  if (phoneString.length >= 6) {
+    phoneString = phoneString.substr(0,6) + "-" + phoneString.substr(6);
+  }
+
+  if (phoneString.length >= 3) {
+    phoneString = "(" + phoneString.substr(0,3) + ") " + phoneString.substr(3);
+  }
+
+  setInputValue(event.currentTarget, phoneString);
+};
+
+function setInputValue(input, value) {
+  var startOffset = input.value.length - input.selectionStart;
+  var endOffset = input.value.length - input.selectionEnd;
+  input.value = value;
+  input.setSelectionRange(value.length - startOffset, value.length - endOffset);
+};
+
+$(document).on('turbolinks:load', function() {
   $('.js-switch').each(function(index, elem) {
-    var init = new Switchery(elem);
+    var init = new Switchery(elem, { size: 'large', secondaryColor: '#efefef', speed: '0.5s' });
   });
 });
