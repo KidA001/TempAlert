f9d3d5017e4fe026a251f1c68a453a3c2549995c
Brian Vogelgesang <KidA001@gmail.com>
2017-07-21 15:00:53 -0700

    Use factory girl in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,2 @@
-Subscriber.create(
-  name: 'Brian',
-  email: 'KidA001@gmail.com',
-  phone: '14157978944',
-  ideal_temperature: 104,
-  subscriptions: {
-    ideal_temp: { sms: true, email: true }
-  }
-)
+FactoryGirl.create(:subscriber, :ideal_temp_subscription)
 Record.create(temperature: 105, recorded_at: Time.now)
