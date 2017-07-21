af8aed2561713082d71aba46cd2d71204c43c847
Brian Vogelgesang <KidA001@gmail.com>
2017-07-21 14:46:30 -0700

    Display message when current temperature is not available

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,7 +2,8 @@ class StatusController < ApplicationController
   using DayCalculator
 
   def index
-    @daytime = Time.now.in_time_zone("Pacific Time (US & Canada)").daytime?
+    @current_time = Time.now.in_time_zone("Pacific Time (US & Canada)")
+    @daytime = @current_time.daytime?
     @current_record = Record.current || Record.new(temperature: 103, recorded_at: Time.now)
   end
 end
