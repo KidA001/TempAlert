4dcaa106818662d45423a5a4fe04ccbf5a368c7e
Brian Vogelgesang <kida001@gmail.com>
2017-07-21 00:31:55 -0700

    Add Back button on settings page Add Settings button on home page Display differnt icons when Day or Night

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,8 @@
 class StatusController < ApplicationController
+  using DayCalculator
+
   def index
+    @daytime = Time.now.in_time_zone("Pacific Time (US & Canada)").daytime?
     @current_record = Record.current || Record.new(temperature: 103, recorded_at: Time.now)
   end
 end
