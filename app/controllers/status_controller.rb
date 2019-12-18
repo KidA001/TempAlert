09d54f35648d67c74166cc8339d3832580d6bc31
Brian Vogelgesang <KidA001@gmail.com>
2019-12-18 12:54:54 -0800

    Refactor daytime calculation

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class StatusController < ApplicationController
-  using DayCalculator
-
   def index
     @current_time = Time.now.in_time_zone("Pacific Time (US & Canada)")
-    @daytime = @current_time.daytime?
+    @daytime = daytime?
     @current_record = Record.current
+  end
+
+  private
+
+  def daytime?
+    now = DateTime.current.in_time_zone(SECRET[:local_timezone])
+    lat, lon = SECRET[:lat_lon].split(',').map(&:to_f)
+    sun_times = SunTimes.new
+    rise = sun_times.rise(now, lat, lon).in_time_zone(SECRET[:local_timezone])
+    set = sun_times.set(now, lat, lon).in_time_zone(SECRET[:local_timezone])
+
+    now.between?(rise, set)
   end
 end
